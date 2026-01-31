e732b73f2fe2ece220f2c2b0ebe82046d2e037ff
Damir Zivkusic <damirzivkusic@Damirs-MacBook-Pro.local>
2026-01-31 21:33:29 +0100

    fixed any errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // 👇 ADD THIS BLOCK
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
